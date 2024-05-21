b020d2675ae8557f3cc0f2bce4d52f796e194a09
shonburgundy <seanbeloussow@yahoo.com>
2024-05-20 17:10:10 -0700

    feat: add context folder and file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
+
   const renderedBooks = books.map((book) => {
     return (
       <BookShow
@@ -12,7 +16,8 @@ function BookList({ books, onDelete, onEdit }) {
     )
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return <div className="book-list">{renderedBooks}
+  {value}</div>;
 }
 
 export default BookList;
